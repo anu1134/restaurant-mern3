132d92945282d8dd6402d3804675e946db8f481a
Anshika Agarwal <anshika@Anshikas-MacBook-Pro.local>
2023-09-03 11:52:22 +0530

    Search Functionality

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -1,27 +1,34 @@
 import RestaurantCard from "./RestaurantCard";
+import SearchComponent from "./Search";
+import { useState } from "react";
 
 const restaurantDetails = [
   {
+    id: "1",
     name: "Bhatura Co",
     avgRating: "4.3",
     cuisines: ["Fast Food, Beverages"],
   },
   {
+    id: "2",
     name: "Big Bowl",
     avgRating: "4.3",
     cuisines: ["Fast Food, Beverages"],
   },
   {
+    id: "3",
     name: "Third Wave Coffee",
     avgRating: "4.3",
     cuisines: ["Fast Food, Beverages"],
   },
   {
+    id: "4",
     name: "Oye Yummy Paratha",
     avgRating: "4.3",
     cuisines: ["Fast Food, Beverages"],
   },
   {
+    id: "5",
     name: "Steamed & Fried",
     avgRating: "4.3",
     cuisines: ["Fast Food, Beverages"],
@@ -29,15 +36,22 @@ const restaurantDetails = [
 ];
 
 const BodyComponent = () => {
+  const [allRestaurants, setAllRestaurants] = useState(restaurantDetails);
+
+  function updateRestaurants(filteredRestaurants) {
+    setAllRestaurants(filteredRestaurants);
+
+    console.log("restaurants....", allRestaurants);
+  }
   return (
     <>
-      <div className="search">
-        <input type="text"></input>
-        <button>Search</button>
-      </div>
+      <SearchComponent
+        restaurants={restaurantDetails}
+        updateRestaurants={updateRestaurants}
+      />
       <div className="res-container">
-        {restaurantDetails.map((res) => {
-          return <RestaurantCard res_details={res} />;
+        {allRestaurants.map((res) => {
+          return <RestaurantCard key={res.id} res_details={res} />;
         })}
       </div>
     </>
---
+++ src/components/Search.js
@@ -0,0 +1,31 @@
+const SearchComponent = (props) => {
+  let searchText = "";
+
+  function filterRestaurants(searchText) {
+    console.log("restaurants", props.restaurants);
+
+    console.log("searchText", searchText);
+
+    let filteredRestaurants = props.restaurants.filter((res) =>
+      res.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    props.updateRestaurants(filteredRestaurants);
+
+    console.log("filteredRestaurants", filteredRestaurants);
+  }
+
+  return (
+    <div className="search">
+      <input
+        type="text"
+        onChange={(e) => {
+          searchText = e.target.value;
+        }}
+      ></input>
+      <button onClick={(e) => filterRestaurants(searchText)}>Search</button>
+    </div>
+  );
+};
+
+export default SearchComponent;
